c57f127fc1db559cda03da2aa4233747f4fa2ef3
Pedro Costa <pedro.costa@gmail.pt>
2021-10-01 15:44:45 +0100

    re-inicial commit

--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
-const { ApolloServer, gql } = require("apollo-server");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const db = require('./db');
 
-const typeDefs = gql`
-  type Query {
-    greeting: String
-  }`;
+const port = 9000;
+const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
-const resolvers = {
-  Query: {
-    greeting: () => "hello graphql user",
-  },
-};
+const app = express();
+app.use(cors(), bodyParser.json(), expressJwt({
+  secret: jwtSecret,
+  credentialsRequired: false
+}));
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+app.post('/login', (req, res) => {
+  const {email, password} = req.body;
+  const user = db.users.list().find((user) => user.email === email);
+  if (!(user && user.password === password)) {
+    res.sendStatus(401);
+    return;
+  }
+  const token = jwt.sign({sub: user.id}, jwtSecret);
+  res.send({token});
 });
 
-server.listen({ port: 9000 })
-  .then((serverInfo) => console.log(`Server running at ${serverInfo.url}`));
+app.listen(port, () => console.info(`Server started on port ${port}`));
